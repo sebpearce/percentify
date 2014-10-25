43b18a5ebf454c46fb3828e81a5b8e07efc710a3
sebpearce <sebpearce@gmail.com>
2014-10-25 19:58:19 +1300

    First commit

---
+++ main.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var inputBox = document.getElementById('number-input');
+var tableDiv = document.getElementById('results-table-container');
+var roundedCheck = document.getElementById('rounded');
+var halfCheck = document.getElementById('half');
+var halfMode = false;
+var roundedMode = false;
+
+function isInt(x) {
+  return (typeof x === 'number' && (x % 1) === 0);
+}
+
+function makeTable (num) {
+
+  var table = '<table class="results-table">';
+
+  for (var i = (halfMode ? 0.5 : 1); i<=num; i = (halfMode ? i+0.5 : i+1)) {
+    var p = (i / num * 100);
+    if (p % 1 != 0) {
+        if (!roundedMode) {
+          p = p.toFixed(1);
+        } else {
+          p = Math.round(p);
+        }
+    }
+
+    table += '<tr>';
+    table += '<td class="score">' + i;
+    table += '<span class="out-of">/' + num + '</span>';
+    table += '</td><td class="pc">' + p + '%</td>';
+    table += '</tr>';
+    if (i % 10 == 0) {
+      table += '</table><table class="results-table">';
+    }
+    if (i % 50 == 0) {
+      table += '</table>';
+      table += '<div style="clear:left;"></div>';
+      table += '<table class="results-table">';
+    }
+  }
+
+  table += '</table>';
+
+  table = table.replace(/\.0%/g,'%');
+
+  return table;
+}
+
+function processInput() {
+  var num = parseInt(inputBox.value);
+  if (isInt(num) && num <= 500) {
+    tableDiv.innerHTML = makeTable(num);
+  }  
+}
+
+function inputHandler(e) {
+  if (e.keyCode == 13) {
+    processInput();
+  }
+  return 0;
+}
+
+function toggle(value) {
+  return (value ? !value : !value);
+}
+
+function halfCheckHandler(e) {
+  if (e.target.checked) {
+    halfMode = true;
+  } else {
+    halfMode = false;
+  }
+  processInput();
+  return 0;
+}
+
+function roundedCheckHandler(e) {
+  if (e.target.checked) {
+    roundedMode = true;
+  } else {
+    roundedMode = false;
+  }
+  processInput();
+  return 0;
+}
+
+inputBox.addEventListener('keypress', inputHandler, false);
+halfCheck.addEventListener('click', halfCheckHandler, false);
+roundedCheck.addEventListener('click', roundedCheckHandler, false);
+
